6b56a6ce3ee08a962bf8acc0882a744187a5dc9e
Joakim Gebart <joakim.gebart@jge.se>
2013-04-29 20:14:27 +0200

    Added logging, split Server and ServerApp

--- server.py
+++ server.py
@@ -2,6 +2,20 @@
 
 # stdin, stdout, stderr
 import sys
+import argparse
+import logging
+# Global logger
+logger = logging.getLogger(__name__)
+
+def set_up_logging():
+    # create console handler and set level
+    ch = logging.StreamHandler()
+    # create formatter
+    formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(funcName)s: %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
 
 class Player(object):
     def __init__(self, player_id, x, y):
@@ -28,31 +42,52 @@ class Bomb(object):
     def __repr__(self):
         return 'Bomb(player_id=%(player_id)d, x=%(x)d, y=%(y)d, ticks=%(ticks)d)' % (self.__dict__)
 
-
-class ServerApp(object):
+class Server(object):
     def __init__(self):
         self.players = list()
         self.bombs = list()
         self.width = 0
         self.height = 0
 
-    def read_map(self):
+    def read_map(self, inpipe):
+        pass
 
-
-    def read_settings(self):
+    def read_settings(self, inpipe):
         # read dimensions
         for var in ('number_of_players', 'max_number_of_turns', 'width', 'height'):
-            line = sys.stdin.readline().split()
-            setattr(self, var, line[0])
+            line = inpipe.readline().split()
+            setattr(self, var, int(line[0]))
 
-        self.read_map()
+        self.read_map(inpipe)
+        self.create_players()
+
+class ServerApp(object):
+    def set_log_level(self, loglevel):
+        numeric_level = getattr(logging, loglevel.upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: %s' % loglevel)
+        # set log level
+        logger.setLevel(numeric_level)
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description='AI server, BombAI competition. (C) Joakim Gebart, Dan Rosenqvist 2013')
+        parser.add_argument('--loglevel', choices=('CRITICAL', 'ERROR', 'WARN', 'INFO', 'DEBUG'),
+                            dest='loglevel', action='store', default='INFO',
+                            help='Log level (default: INFO)')
+        self.args = parser.parse_args()
+        self.set_log_level(self.args.loglevel)
 
     def run(self):
-        self.read_settings()
+        logger.info('Starting...')
+        self.parse_args()
 
+        logger.debug('Instantiating server...')
+        self.server = Server()
 
-
+        logger.debug('Reading server settings...')
+        self.server.read_settings(sys.stdin)
 
 if __name__ == '__main__':
+    set_up_logging()
     app = ServerApp()
     app.run()
