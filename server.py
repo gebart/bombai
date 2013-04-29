b3b1157fcebdecdfbf6eefe0e86d5e091fc63e52
Joakim Gebart <joakim.gebart@jge.se>
2013-04-29 19:24:08 +0200

    App skeleton

---
+++ server.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+
+# stdin, stdout, stderr
+import sys
+
+class ServerApp(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    app = ServerApp()
+    app.run()
