eb28159b5d4f4c3bffe3aee16e5d89b83f972350
dan <dan@themis.(none)>
2013-04-29 20:28:48 +0200

    WIP

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 # stdin, stdout, stderr
 import sys
+import numpy as np
 
 class Player(object):
     def __init__(self, player_id, x, y):
@@ -28,6 +29,20 @@ class Bomb(object):
     def __repr__(self):
         return 'Bomb(player_id=%(player_id)d, x=%(x)d, y=%(y)d, ticks=%(ticks)d)' % (self.__dict__)
 
+class Map(object):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.map = np.empty((self.height, self.width))
+        
+    def read_map(self):
+		for line_n in range(0, self.height):
+            line = sys.stdin.readline()
+			for col_n in range(0, line.size):
+                map[line_n, col_n] = line[col_n]
+        
+    def __str__(self):
+        return 'Current map=\n%s' % (repr(self.map),)
 
 class ServerApp(object):
     def __init__(self):
@@ -35,17 +50,15 @@ class ServerApp(object):
         self.bombs = list()
         self.width = 0
         self.height = 0
-
-    def read_map(self):
-
+        self.map = None
 
     def read_settings(self):
         # read dimensions
         for var in ('number_of_players', 'max_number_of_turns', 'width', 'height'):
             line = sys.stdin.readline().split()
             setattr(self, var, line[0])
-
-        self.read_map()
+        self.map = Map(height, width)
+        self.map.read_map()
 
     def run(self):
         self.read_settings()
